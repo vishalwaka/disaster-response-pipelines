0b0182c3765c6991ec7fa5b66fbdcd68374a6232
Vishal Madheshia <vishalmadheshia@gmail.com>
2020-04-12 02:59:09 +0000

    Complete ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,57 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    
+    # load messages and categories dataset
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
 
+    # merge datasets
+    df = messages.merge(categories, on=["id"])
+    return df
 
 def clean_data(df):
-    pass
+    
+    # create a dataframe of the 36 individual category columns
+    categories = df["categories"].str.split(";", expand = True)
 
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    slice_column = lambda name: name.split("-")[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(slice_column)
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype("int")
+        
+    # drop the original categories column from `df`
+    df.drop("categories", axis = 1, inplace = True)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis = 1)
+    
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+    
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    
+    # Save the clean dataset into an sqlite database
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    df.to_sql('MessageTable', engine, index=False)  
 
 
 def main():
